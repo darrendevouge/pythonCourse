d5f2706d51ea9e73736d013b427a0ec826500c02
Darren De Vouge <darrn.dv@gmail.com>
2023-10-22 01:23:25 -0400

    operators

---
+++ lesson03/practice/meaning.py
@@ -0,0 +1,27 @@
+print(24 // 5) # floor division -- The answer is rounded down
+
+print(round(24 / 5)) #rounds up
+
+print(24 % 5) #Gives the remainder only
+
+print(2**3) #2 to the power of 3
+
+meaning = 42
+
+meaning +=1
+print(meaning)
+
+meaning -=1
+print(meaning)
+
+print('')
+
+# if meaning > 10:
+#     print('Right on!')
+# else:
+#     print('Not today')
+
+# Ternary Operator
+print('Right on!') if meaning > 10 else print('Not today')
+
+
